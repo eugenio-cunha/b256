986c9fbd8c199570deee0edbd26fc3ade59aa03d
eugenio.cunha <eugenio.cunha@ymail.com>
2024-10-07 19:22:37 -0300

    main - refactor: changed SettingsTest

--- core/model/src/test/java/br/com/b256/core/model/SettingsTest.kt
+++ core/model/src/test/java/br/com/b256/core/model/SettingsTest.kt
@@ -3,34 +3,25 @@ package br.com.b256.core.model
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 
 class SettingsTest {
+
+    private lateinit var settings: Settings
+
+    @Before
+    fun setup() {
+        settings = Settings(biometrics = true, theme = Theme.LIGHT)
+    }
+
     @Test
     fun settings_biometrics_true() {
-        val settings = Settings(biometrics = true, theme = Theme.LIGHT)
-
         assertEquals(settings.biometrics, true)
     }
 
     @Test
-    fun settings_biometrics_false() {
-        val settings = Settings(biometrics = false, theme = Theme.LIGHT)
-
-        assertEquals(settings.biometrics, false)
-    }
-
-    @Test
     fun settings_theme_light() {
-        val settings = Settings(biometrics = false, theme = Theme.LIGHT)
-
         assertEquals(settings.theme, Theme.LIGHT)
-    }
-
-    @Test
-    fun settings_theme_dark() {
-        val settings = Settings(biometrics = false, theme = Theme.DARK)
-
-        assertEquals(settings.theme, Theme.DARK)
     }
 }
 
