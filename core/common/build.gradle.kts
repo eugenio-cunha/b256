7e1395af869313d4b6e8f7d5e359d41b74dbe396
eugenio.cunha <eugenio.cunha@ymail.com>
2024-10-16 22:02:48 -0300

    main - fix: test modulo common

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -11,7 +11,11 @@ dependencies {
     api(libs.kotlinx.datetime)
     api(libs.kotlinx.coroutines.core)
 
-    testImplementation(libs.kotlin.test)
-    testImplementation(libs.kotlinx.coroutines.test)
     testImplementation(libs.turbine)
+    testImplementation(libs.kotlin.test)
+    testImplementation(libs.hilt.android.testing)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.androidx.compose.ui.test)
+    testImplementation(libs.androidx.compose.ui.testManifest)
+    androidTestImplementation(libs.bundles.androidx.compose.ui.test)
 }
