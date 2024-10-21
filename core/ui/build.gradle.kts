6b37bd1eb3936b619ce25aa9e803c7012a1fbca6
eugenio.cunha <eugenio.cunha@ymail.com>
2024-10-21 20:43:20 -0300

    main - feat: implementei um componente de camera

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -11,9 +11,15 @@ dependencies {
     api(libs.kotlinx.datetime)
     implementation(projects.core.designsystem)
 
+    implementation(libs.androidx.camera)
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.view)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.extension)
+    implementation(libs.androidx.browser)
+    implementation(libs.accompanist.permissions)
     implementation(libs.coil.kt)
     implementation(libs.coil.kt.compose)
-    implementation(libs.androidx.browser)
 
     androidTestImplementation(libs.bundles.androidx.compose.ui.test)
 }
