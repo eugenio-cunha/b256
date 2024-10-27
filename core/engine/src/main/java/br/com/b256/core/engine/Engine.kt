2c052aceb61fa5584d918c11b22f25c3843e72e9
eugenio.cunha <eugenio.cunha@ymail.com>
2024-10-27 12:11:29 -0300

    native - feat: todo

--- core/engine/src/main/java/br/com/b256/core/engine/Engine.kt
+++ core/engine/src/main/java/br/com/b256/core/engine/Engine.kt
@@ -3,6 +3,7 @@ package br.com.b256.core.engine
 import android.graphics.Bitmap
 
 class Engine {
+    //TODO: olhar este repositorio https://github.com/cubukcum/lane-detection-kotlin-opencv/blob/main/app/build.gradle
 
     /**
      * A native method that is implemented by the 'engine' native library,
